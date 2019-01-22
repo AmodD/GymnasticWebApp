85786a797c545a8e7de4f7e25eb2ac87bb527855
Ajinkya <ajinkyameister@gmail.com>
2019-01-22 21:45:46 +0530

    no change made

--- resources/js/components/Message.vue
+++ resources/js/components/Message.vue
@@ -2,11 +2,13 @@
 		<div>
 <li v-for="student in students"> 
 	<div class="card">
-		<div class="card-content">	{{student.name}} </div>
-		
+		<div class="card-content">	{{student.name}} </div>		
 	</div>
-	<button class="button is-primary" type="submit" name="attendance" value=true>Present</button>
-	<button  class="button is-danger" type="submit" name="attendance" value=false>Absent</button>
+	<form action="/attendances" method="POST">
+						{{csrf_field()}}
+		<button class="button is-primary" type="submit" name="attendance" value=true>Present</button>
+		<button  class="button is-danger" type="submit" name="attendance" value=false>Absent</button>
+	</form>
 </li> 
 </div>
 						
