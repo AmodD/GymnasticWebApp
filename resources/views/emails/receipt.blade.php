74674be501a1f6a0687a15792d8b9bffd2650521
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2019-03-23 23:20:54 +0530

    added the word only in receipt

--- resources/views/emails/receipt.blade.php
+++ resources/views/emails/receipt.blade.php
@@ -5,7 +5,7 @@
 Date: <b>{{ $fee->date }}</b><br>
 Received with thanks from : <b>{{ $fee->student->name }} </b><br>
 The Sum of Rupees : <b>{{ $fee->amount }}</b><br>
-In Words : <b>Rupees {{ ucwords( (new NumberFormatter("en", NumberFormatter::SPELLOUT) )->format($fee->amount) ) }}</b><br>
+In Words : <b>Rupees {{ ucwords( (new NumberFormatter("en", NumberFormatter::SPELLOUT) )->format($fee->amount) ) }} Only</b><br>
 Via : <b>{{ $fee->mode }}</b><br>
 Comments : <b>{{ $fee->comments }}</b><br>
 For the Period : <b>{{ $fee->period }}</b><br>
